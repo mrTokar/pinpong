6be95f908e0747bc7cd65eee5caedd3277e5f806
Tokar <egortok2014@gmail.com>
2022-09-14 00:07:38 +0300

    You can play now
    Add:
    - working rockets
    - score
    - restart round

--- main.py
+++ main.py
@@ -20,26 +20,53 @@ class PinPongBall(Widget):
         self.pos = Vector(*self.speed) + self.pos
 
 
+class PinPongRacket(Widget):
+    score = NumericProperty(0)
+
+    def bounce_ball(self, ball):
+        if self.collide_widget(ball):
+            sx, sy = ball.speed
+            bounced = Vector(-1 * sx, sy) * 1.1
+            ball.speed = bounced.x, bounced.y
+
+
 class PinPongGame(Widget):
     # link on ball in kv file
     ball = ObjectProperty(None)
+    # link on rackets in kv file
+    pc_racket = ObjectProperty(None)
+    player_racket = ObjectProperty(None)
 
-    def restart_ball(self):
-        """restart ball and give him random speed"""
+    def restart_ball(self, vec=(5, 0)):
+        """restart ball and give him start speed(vec)"""
         self.ball.center = self.center
-        self.ball.speed = Vector(4,0).rotate(randint(0, 360))
+        self.ball.speed = Vector(vec).rotate(randint(-70, 70))
 
     def update(self, dt):
         """Call ball.move and other"""
         self.ball.move()
 
+        # bounce off racket
+        self.pc_racket.bounce_ball(self.ball)
+        self.player_racket.bounce_ball(self.ball)
         # bounce off top and bottom
         if (self.ball.y < 0) or (self.ball.top > self.height):
             self.ball.speed_y *= -1
 
         # bounce off left and right 
-        if (self.ball.x < 0) or (self.ball.right > self.width):
-            self.ball.speed_x *+ -1
+        elif self.ball.x < self.x:
+            self.player_racket.score += 1
+            self.restart_ball(vec=(5,0))
+        elif self.ball.x > self.width:
+            self.pc_racket.score += 1
+            self.restart_ball(vec=(-5,0))
+
+    def on_touch_move(self, touch):
+        if touch.x > self.width - self.width/3:
+            self.player_racket.center_y = touch.y
+        #===========DEL IT=======================
+        if touch.x < self.width/3:
+            self.pc_racket.center_y = touch.y   
 
 
 class PinPongApp(App):
