0401d27810681eaf0a647bb21a7f42c4e9167360
Tokar <egortok2014@gmail.com>
2022-09-22 23:32:36 +0300

    Add menu
    Include 3 buttons: Start, Setting*, Exit
    
    - Start => start the game
    - Exit => Clance App
    - Setting => Open Settings Menu
    
    Also, if you can press on Esc, then Menu will open and game will stop.
    *Setting hasn't worked yet.

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
 from kivy.vector import Vector
 from kivy.clock import Clock
 from kivy.properties import (
-    NumericProperty, ReferenceListProperty, ObjectProperty
+    NumericProperty, ReferenceListProperty, ObjectProperty, ListProperty
 )
 from kivy.core.window import Window
 from random import randint
+from sys import exit
 
+Window.minimum_width = 650
+Window.minimum_height = 535
 
 class PinPongRacket(Widget):
     score = NumericProperty(0)
@@ -34,6 +38,7 @@ class ComputerPlayer:
             self.flag_move = False
 
     def change_speed(self, speed_y):
+        """Change speed PC_racket"""
         self.speed = 0.7 * speed_y
 
     def move_racket(self):
@@ -66,9 +71,13 @@ class PinPongBall(Widget):
         self.pc = pc
 
     def on_speed_x(self, refproperty, value):
+        """Observation of the value speed_x. 
+        Switch for ComputerPlayer"""
         self.pc.change_flag(value, self.speed_y)
 
     def on_speed_y(self, refpropert, value):
+        """Observation of the value speed_y.
+        Change speed racket."""
         if self.pc.flag_move:
             self.pc.change_speed(self.speed_y)
 
@@ -79,6 +88,19 @@ class PinPongGame(Widget):
     # link on rackets in kv file
     pc_racket = ObjectProperty(None)
     player_racket = ObjectProperty(None)
+    menu = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._keyboard = Window.request_keyboard(lambda: None, self)
+        if self._keyboard.widget:
+            pass
+        self._keyboard.bind(on_key_down=self._on_keyboard_down)
+
+    def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
+        if keycode[1] == "escape":
+            self.menu.open()
+        return True
 
     def restart_ball(self, vec=(5, 0)):
         """restart ball and give him start speed(vec)"""
@@ -107,6 +129,7 @@ class PinPongGame(Widget):
             self.restart_ball(vec=(-5,0))
 
     def on_touch_move(self, touch):
+        """Toch_move Event. Racket control"""
         h = self.player_racket.height / 2 
         if touch.x > self.width - self.width/3:
             if h <= touch.y <= self.height - h:
@@ -114,14 +137,43 @@ class PinPongGame(Widget):
             elif h > touch.y:
                 self.player_racket.center_y = h
             else:
-                self.player_racket.center_y = self.height - h  
+                self.player_racket.center_y = self.height - h
+    
+
+class Menu(BoxLayout):
+    def init_root_widget(self):
+        """Search root widget and save its link to attribute"""
+        for w in self.walk_reverse(): 
+            if isinstance(w, PinPongGame):
+                self.game = w
+
+    def start(self):
+        """Start game"""
+        self.pos = self.size[0] * -1, self.size[1] * -1  # move menu
+        try:
+            self.game.restart_ball()
+        except AttributeError:
+            self.init_root_widget()
+            self.game.restart_ball()
+        self.clock = Clock.schedule_interval(self.game.update, 1.0 / 100.0)
+
+    def exit_app(self):
+        exit(0)
+
+    def setting(self):
+        print(self.size, self.pos)
+        print("window", Window.size)
+
+    def open(self):
+        """Pause game and open menu"""
+        self.clock.cancel()
+        self.game.restart_ball(vec=(0,0))
+        self.pos = 0.05*self.game.width/2, 0.10*self.game.width/2
 
 
 class PinPongApp(App):
     def build(self):
         game = PinPongGame()
-        game.restart_ball()
-        Clock.schedule_interval(game.update, 1.0 / 60.0) 
         return game
 
 
